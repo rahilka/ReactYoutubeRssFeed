706c7510148181e16e7aac47b94b6d81b09f415e
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-10-28 16:08:26 +0200

    Added styles to the video. Removed unused files

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,14 +3,14 @@ import React from 'react';
 const VideoDetail = (props) => {
 	const {title, description, videoId} = props.location.state;
 
-  const url = `https://www.youtube.com/embed/${videoId}`; //es6 feature: template strings
+  const url = `https://www.youtube.com/embed/${videoId}`;
 
 	return (
 		<div className = "video-detail col-md-8">
       <div className = "embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url} title={title}></iframe>
       </div>
-      <div className = "details">
+      <div className="details">
         <div>{title}</div>
         <div>{description}</div>
       </div>
